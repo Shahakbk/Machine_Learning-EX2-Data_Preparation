a1160774f22910e439a00ac15b67fa45785411a9
Shahakbk <shahakbk@gmail.com>
2019-05-01 21:04:51 +0300

    some re-ordering of the steps and added imputation (predicting doesn't work yet)

--- elections.py
+++ elections.py
@@ -5,58 +5,143 @@ from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectPercentile
 from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict, StratifiedShuffleSplit
 from sklearn.impute import SimpleImputer
-# from sklearn.impute import IterativeImputer //TODO check why this doesn't work.
-from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import MissingIndicator
+from sklearn import preprocessing
+from sklearn.pipeline import make_pipeline
+from skrebate import ReliefF
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier
+import utils
 
 # Loading the data.
 # data_url = 'https://grades.cs.technion.ac.il/grades.cgi?dbbceceead9ba0238f47df22b94b49+2+236756+Spring2019+hw/WCFiles/ElectionsData.csv+7366'
 # dataset = pd.read_csv(data_url, header=0)
-dataset = pd.read_csv('ElectionsData.csv', header=0)
+dataset = pd.read_csv('ElectionsData_orig.csv', header=0)
+dataset = dataset.replace([np.inf, -np.inf], np.nan)
 
-# Converting binary features to 0 / 1 representation.
-dataset['Looking_at_poles_results_Numeric'] = dataset['Looking_at_poles_results'].map({'No': 0, 'Yes': 1, np.nan: -1}).astype(int)
-dataset['Married_Numeric'] = dataset['Married'].map({'No': 0, 'Yes': 1, np.nan: -1}).astype(int)
-dataset['Gender_Numeric'] = dataset['Gender'].map({'Male': 0, 'Female': 1, np.nan: -1}).astype(int)
-dataset['Voting_Time_Numeric'] = dataset['Voting_Time'].map({'By_16:00': 0, 'After_16:00': 1, np.nan: -1}).astype(int)
-dataset['Financial_agenda_matters_Numeric'] = dataset['Financial_agenda_matters'].map({'No': 0, 'Yes': 1, np.nan: -1}).astype(int)
+"""
+Marking missing values.
+"""
+# Create a DF to hold True/False for indicating whether or not values were initially empty.
+dataset_nan = dataset.isnull()
+# Temporarily fill the missing values with '0'.
+dataset = dataset.fillna(0)
 
-# Dropping the previous non-numerical columns.
-dataset = dataset.drop(['Looking_at_poles_results', 'Married', 'Gender', 'Voting_Time', 'Financial_agenda_matters'], axis=1)
-
-# Converting the 'Vote' column to numerical.
-dataset['Vote'] = dataset['Vote'].map({'Blues': 0, 'Browns': 1, 'Greens': 2, 'Greys': 3, 'Khakis': 4,
-                                               'Oranges': 5, 'Pinks': 6, 'Purples': 7, 'Reds': 8, 'Turquoises': 9,
-                                               'Violets': 10, 'Whites': 11, 'Yellows': 12}).astype(int)
-# Y will be the 'Vote' column (the label).
+# Y will be the 'Vote' column (the label) and X will be the rest of the columns.
 y = dataset['Vote']
-
-# Converting categorical features to OneHot representation.
-dataset_dummies = pd.get_dummies(data=dataset, columns=['Most_Important_Issue', 'Main_transportation', 'Occupation'])
-dataset = pd.concat([dataset, dataset_dummies], axis=1)
-
-# Dropping the previous non-numerical columns.
-dataset = dataset.drop(['Most_Important_Issue', 'Main_transportation', 'Occupation'], axis=1)
-
-# X will be the rest of the columns.
 X = dataset.drop(['Vote'], axis=1)
+
+"""
+Splitting the data.
+"""
 
 # Splitting the data into train, validation and test sets divided randomly to 60% : 20% : 20%.
 # Since train_test_split only splits into two sets, the procedure will be done twice.
 
-# First step - splitting the whole set into train set & test set.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+# First step - splitting the whole set into train set & a temporary set.
+indices = range(X.shape[0])
+X_train, X_tmp, y_train, y_tmp, indices_train, indices_tmp = train_test_split(X, y, indices, test_size=0.4, random_state=1)
 
-# Second step - splitting the test set into test set & validation set.
-X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=1)
+# Second step - splitting the the temporary set into test set & validation set.
+indices = range(X_tmp.shape[0])
+X_test, X_val, y_test, y_val, indices_test, indices_val = train_test_split(X_tmp, y_tmp, indices, test_size=0.5, random_state=1)
+
+# Correcting the indices of the train & validation sets.
+for i in range(len(indices_test)):
+    indices_test[i] = indices_tmp[indices_test[i]]
+
+for i in range(len(indices_val)):
+    indices_val[i] = indices_tmp[indices_val[i]]
+
+"""
+Converting non-numeric data.
+"""
+
+train_data = pd.concat([X_train, y_train], axis=1)
+val_data = pd.concat([X_val, y_val], axis=1)
+test_data = pd.concat([X_test, y_test], axis=1)
+
+train_data = utils.convert_discrete_features(train_data)
+val_data = utils.convert_discrete_features(val_data)
+test_data = utils.convert_discrete_features(test_data)
+
+X_train = train_data.drop(['Vote'], axis=1)
+y_train = train_data['Vote']
+
+X_val = val_data.drop(['Vote'], axis=1)
+y_val = val_data['Vote']
+
+X_test = test_data.drop(['Vote'], axis=1)
+y_test = test_data['Vote']
+
+"""
+Imputing missing values with regression/ classification decision trees.
+"""
+
+# Create a 'clean' train dataset without any NaN values.
+train_data_no_nan = pd.concat([X_train, y_train], axis=1)
+train_data_no_nan = train_data_no_nan.dropna()
+y_train_no_nan = train_data_no_nan['Vote']
+X_train_no_nan = train_data_no_nan.drop(['Vote'], axis=1)
+
+pd.DataFrame.to_csv(X_train, 'X_train.csv')
+pd.DataFrame.to_csv(X_val, 'X_val.csv')
+pd.DataFrame.to_csv(X_test, 'X_test.csv')
+pd.DataFrame.to_csv(y_train, 'y_train.csv')
+
+
+discrete_features = ['Occupation_Satisfaction', 'Most_Important_Issue', 'Looking_at_poles_results', 'Married', 'Gender',
+                     'Voting_Time', 'Will_vote_only_large_party', 'Last_school_grades', 'Age_group',
+                     'Number_of_differnt_parties_voted_for', 'Number_of_valued_Kneset_members', 'Main_transportation',
+                     'Occupation', 'Num_of_kids_born_last_10_years', 'Financial_agenda_matters']
+continuous_features = ['Avg_monthly_expense_when_under_age_21', 'AVG_lottary_expanses',
+                       'Avg_monthly_expense_on_pets_or_plants', 'Avg_environmental_importance',
+                       'Financial_balance_score_(0-1)', '%Of_Household_Income', 'Yearly_IncomeK',
+                       'Overall_happiness_score', 'Garden_sqr_meter_per_person_in_residancy_area',
+                       'Avg_Residancy_Altitude', 'Yearly_ExpensesK', '%Time_invested_in_work',
+                       'Avg_government_satisfaction', 'Avg_Satisfaction_with_previous_vote',
+                       'Avg_monthly_household_cost', 'Phone_minutes_10_years', 'Avg_size_per_room',
+                       'Weighted_education_rank', '%_satisfaction_financial_policy', 'Avg_monthly_income_all_years',
+                       'Political_interest_Total_Score', 'Avg_education_importance']
+
+for feature in continuous_features:
+    clf = DecisionTreeRegressor(criterion="mse")
+    clf.fit(X_train_no_nan, y_train_no_nan)
+
+    for idx in range(len(indices_train)):
+        # This means the sample originally had a NaN value in this feature.
+        if dataset_nan[feature][indices_train[idx]] == True:
+            sample = X_train.iloc[idx, :]
+            X_train[feature][indices_train[idx]] = clf.predict(X_train.iloc[idx, :])
+
+
+for feature in discrete_features:
+    clf = DecisionTreeClassifier(criterion="entropy")
+    clf.fit(X_train_no_nan, y_train_no_nan)
+
+    # TODO check 'Numerical' columns
+    for idx in range(len(indices_train)):
+        # This means the sample originally had a NaN value in this feature.
+        if dataset_nan[feature][indices_train[idx]] == True:
+            sample = X_train.iloc[idx, :]
+            X_train[feature][indices_train[idx]] = clf.predict(X_train.iloc[idx, :])
+
+
+
+"""
+# First of all, we try to find correlation between features in order for the imputing to be as accurate as possible.
+corr_matrix = dataset.corr(method='pearson', min_periods=1)
 
 # TODO this should probably be changed based on correlation
 # Imputing the missing data.
 imp = SimpleImputer(strategy="most_frequent")
 X_train = imp.fit_transform(X_train)
 
+"""
 
 # Detecting outliers.
 # TODO in order for this to work, we first need to convert non-numerical features into numbers.
@@ -82,6 +167,21 @@ def plot_2d_space(X, y, label):
 
 """
 # Plotting the data.
-plot_2d_space(X_test, y_train, 'Train data - Labels distribution')
+# plot_2d_space(X_train, y_train, 'Train data - Labels distribution')
+"""
+
+"""
+# Performing features selection.
+# Starting with ReliefF algorithm as a filter method.
+
+knn = KNeighborsClassifier(n_neighbors=4)
+sfs = SFS(knn, k_features=3, forward=True, floating=False, scoring='accuracy', cv=0)
+sfs = sfs.fit(X_train, y_train)
+
+# TODO set threshold based on the biggest categorical feature.
+# 'discrete_threshold' is set to 4 so that any feature with over 13 unique values will be considered continuous.
+# 'n_jobs' is set to -1 so that all available cores will be used for the algorithm.
+relieff = ReliefF(n_features_to_select=3, n_neighbors=100, discrete_threshold=13)
+X_train_reduced = relieff.fit_transform(X_train, y_train)
 
 """
